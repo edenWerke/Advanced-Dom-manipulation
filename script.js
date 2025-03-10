e7666c01212dc2716ec86f9ba727a8889fc290d0
edenWerke <edenw6292@gmail.com>
2025-03-10 16:56:39 +0300

    removing event listener with setting timeout

--- script.js
+++ script.js
@@ -177,7 +177,7 @@ const alertH1=function(e){
 
 h1.addEventListener('mouseenter',alertH1);
 h1.removeEventListener('mouseenter',alertH1)
-// setTimeout(()=>h1.removeEventListener('mouseenter',alertH1),3000)
+setTimeout(()=>h1.removeEventListener('mouseenter',alertH1),3000)
 // h1.onmouseenter=function(e) {
 //   alert('addEventListener: Great! You are reading the heading :D');
 // };
