360412b4ce26f3bb0fda7ab0f294b7c4a5446177
edenWerke <edenw6292@gmail.com>
2025-03-11 09:16:10 +0300

    e.target

--- script.js
+++ script.js
@@ -188,12 +188,16 @@ const randomInt=(min,max)=>Math.floor(Math.random()*max-min)+1;
 const randomColor=()=>`rgb(${randomInt(0,255)},${randomInt(0,255)},${randomInt(0,255)})`
 console.log(randomColor(0,255))
 
+
 document.querySelector('.nav__link').addEventListener('click',function(e){
   this.style.backgroundColor=randomColor();
+  console.log(e.target)
 })
 document.querySelector('.nav__links').addEventListener('click',function(e){
   this.style.backgroundColor=randomColor();
+  console.log(e.target)
 })
 document.querySelector('.nav').addEventListener('click',function(e){
   this.style.backgroundColor=randomColor();
+  console.log(e.target)
 })
