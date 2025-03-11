ce7c2b5e046c27ce9b93d654f9a351c6872d18b8
edenWerke <edenw6292@gmail.com>
2025-03-11 22:18:43 +0300

    smooth scroling

--- script.js
+++ script.js
@@ -62,6 +62,10 @@ document.querySelectorAll('.nav__link').forEach(function(el){
   el.addEventListener('click',function(e){
     e.preventDefault();
     console.log("my link ")
+
+    const id=this.getAttribute('href')
+    console.log(id)
+    document.querySelector(id).scrollIntoView({behavior:'smooth'})
   })
 })
 
